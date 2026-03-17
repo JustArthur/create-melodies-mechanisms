7b6c34e6bedbd5191c35d1f25c0049762de7fde3
su apt install kuma <maxxozou@gmail.com>
2026-03-17 10:49:25 +0100

    Make disc plates unstackable and update mall recipe
    Set FabricItemSettings.maxCount(1) for all disc plate items so they are non-stackable and remove their additions from the ingredient item group. Also replace the amethyst_shard ingredient with purple_dye in the mall sequenced_assembly recipes across fabric, forge, and neoforge directories.

--- fabric-1.20.1/src/main/java/net/suaptinstallkuma/create_melodies_mechanisms/item/ModItems.java
+++ fabric-1.20.1/src/main/java/net/suaptinstallkuma/create_melodies_mechanisms/item/ModItems.java
@@ -16,44 +16,27 @@ public class ModItems {
     public static final Item NETHERRACK_DISC_PLATE = registerItem("netherrack_disc_plate", new Item(new FabricItemSettings()));
     public static final Item TERRACOTTA_DISC_PLATE = registerItem("terracotta_disc_plate", new Item(new FabricItemSettings()));
 
-    public static final Item BLOCKS_DISC_PLATE = registerItem("blocks_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item CAT_DISC_PLATE = registerItem("cat_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item CHIRP_DISC_PLATE = registerItem("chirp_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item ELEVEN_DISC_PLATE = registerItem("eleven_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item FAR_DISC_PLATE = registerItem("far_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item FIVE_DISC_PLATE = registerItem("five_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item MALL_DISC_PLATE = registerItem("mall_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item MELLOHI_DISC_PLATE = registerItem("mellohi_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item OTHERSIDE_DISC_PLATE = registerItem("otherside_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item PIGSTEP_DISC_PLATE = registerItem("pigstep_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item RELIC_DISC_PLATE = registerItem("relic_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item STAL_DISC_PLATE = registerItem("stal_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item STRAD_DISC_PLATE = registerItem("strad_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item THIRTEEN_DISC_PLATE = registerItem("thirteen_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item WAIT_DISC_PLATE = registerItem("wait_disc_plate", new Item(new FabricItemSettings()));
-    public static final Item WARD_DISC_PLATE = registerItem("ward_disc_plate", new Item(new FabricItemSettings()));
+    public static final Item BLOCKS_DISC_PLATE = registerItem("blocks_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item CAT_DISC_PLATE = registerItem("cat_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item CHIRP_DISC_PLATE = registerItem("chirp_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item ELEVEN_DISC_PLATE = registerItem("eleven_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item FAR_DISC_PLATE = registerItem("far_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item FIVE_DISC_PLATE = registerItem("five_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item MALL_DISC_PLATE = registerItem("mall_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item MELLOHI_DISC_PLATE = registerItem("mellohi_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item OTHERSIDE_DISC_PLATE = registerItem("otherside_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item PIGSTEP_DISC_PLATE = registerItem("pigstep_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item RELIC_DISC_PLATE = registerItem("relic_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item STAL_DISC_PLATE = registerItem("stal_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item STRAD_DISC_PLATE = registerItem("strad_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item THIRTEEN_DISC_PLATE = registerItem("thirteen_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item WAIT_DISC_PLATE = registerItem("wait_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
+    public static final Item WARD_DISC_PLATE = registerItem("ward_disc_plate", new Item(new FabricItemSettings().maxCount(1)));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(DEEPSLATE_DISC_PLATE);
         entries.add(NETHERRACK_DISC_PLATE);
         entries.add(TERRACOTTA_DISC_PLATE);
-
-        entries.add(BLOCKS_DISC_PLATE);
-        entries.add(CAT_DISC_PLATE);
-        entries.add(CHIRP_DISC_PLATE);
-        entries.add(ELEVEN_DISC_PLATE);
-        entries.add(FAR_DISC_PLATE);
-        entries.add(FIVE_DISC_PLATE);
-        entries.add(MALL_DISC_PLATE);
-        entries.add(MELLOHI_DISC_PLATE);
-        entries.add(OTHERSIDE_DISC_PLATE);
-        entries.add(PIGSTEP_DISC_PLATE);
-        entries.add(RELIC_DISC_PLATE);
-        entries.add(STAL_DISC_PLATE);
-        entries.add(STRAD_DISC_PLATE);
-        entries.add(THIRTEEN_DISC_PLATE);
-        entries.add(WAIT_DISC_PLATE);
-        entries.add(WARD_DISC_PLATE);
     }
 
     private static Item registerItem(String name, Item item) {
